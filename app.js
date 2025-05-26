3e49a5d5284906b2293d44ce28cce5ed26582f4d
cealpa <cealpa@gmail.com>
2025-05-26 20:47:29 +0200

    editado

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
+<<<<<<< HEAD
   res.send('Hello Caracola World!\n');
+=======
+  res.send('HelloCaracolillo World!\n');
+>>>>>>> b396441 (editado)
 });
 
 app.listen(8080, function () {
